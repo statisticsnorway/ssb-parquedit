a65d7ec10202ca7a26c98ae5be1b53d0d6582a84
vidstra <vst@ssb.no>
2026-03-06 09:24:06 +0100

    added tests for functions.py

---
+++ tests/test_functions.py
@@ -0,0 +1,48 @@
+import os
+import pytest
+from unittest.mock import patch
+from ssb_parquedit.functions import get_dapla_group, get_team_name, get_bucket_name
+
+
+class TestGetDaplaGroup:
+    def test_returns_env_variable(self):
+        with patch.dict(os.environ, {'DAPLA_GROUP_CONTEXT': 'dapla-ffunk-developers'}):
+            assert get_dapla_group() == 'dapla-ffunk-developers'
+
+    def test_returns_empty_string_when_not_set(self):
+        with patch.dict(os.environ, {}, clear=True):
+            assert get_dapla_group() == ""
+
+
+class TestGetTeamName:
+    def test_extracts_team_name(self):
+        with patch.dict(os.environ, {'DAPLA_GROUP_CONTEXT': 'dapla-ffunk-developers'}):
+            assert get_team_name() == 'dapla-ffunk'
+
+    def test_returns_empty_string_when_not_set(self):
+        with patch.dict(os.environ, {}, clear=True):
+            assert get_team_name() == ""
+
+    def test_handles_short_group_name(self):
+        with patch.dict(os.environ, {'DAPLA_GROUP_CONTEXT': 'dapla'}):
+            assert get_team_name() == 'dapla'
+
+
+class TestGetBucketName:
+    def test_builds_bucket_name(self):
+        with patch.dict(os.environ, {
+            'DAPLA_GROUP_CONTEXT': 'dapla-ffunk-developers',
+            'DAPLA_ENVIRONMENT': 'TEST'
+        }):
+            assert get_bucket_name() == 'ssb-dapla-ffunk-data-produkt-test'
+
+    def test_environment_is_lowercased(self):
+        with patch.dict(os.environ, {
+            'DAPLA_GROUP_CONTEXT': 'dapla-ffunk-developers',
+            'DAPLA_ENVIRONMENT': 'PROD'
+        }):
+            assert get_bucket_name() == 'ssb-dapla-ffunk-data-produkt-prod'
+
+    def test_returns_empty_environment_when_not_set(self):
+        with patch.dict(os.environ, {'DAPLA_GROUP_CONTEXT': 'dapla-ffunk-developers'}, clear=True):
+            assert get_bucket_name() == 'ssb-dapla-ffunk-data-produkt-'
