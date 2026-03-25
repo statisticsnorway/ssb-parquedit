d0034b2679938f26b369d92cd958af406e4a2230
ssb-vst <vst@ssb.no>
2026-03-25 13:26:21 +0100

    added inlining and pinned duckdb to 1.5.1

--- src/ssb_parquedit/functions.py
+++ src/ssb_parquedit/functions.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 
 def get_dapla_group() -> str:
@@ -78,3 +79,39 @@ def create_config() -> dict[str, str]:
     }
 
     return db_config
+
+
+def repo_root_dir(directory: Path | str | None = None) -> Path:
+    """Find the root directory of a git repo, searching upwards from a given path.
+
+    Args:
+        directory: The path to search from, defaults to the current working directory.
+            The directory can be of type string or of type pathlib.Path.
+
+    Returns:
+        Path to the git repo's root directory.
+
+    Raises:
+        RuntimeError: If no .git directory is found when searching upwards.
+
+    Example:
+    --------
+    >>> import dapla as dp
+    >>> import tomli
+    >>>
+    >>> config_file = dp.repo_root_dir() / "pyproject.toml"
+    >>> with open(config_file, mode="rb") as fp:
+    >>>     config = tomli.load(fp)
+    """
+    if directory is None:
+        directory = Path.cwd()
+
+    if isinstance(directory, str):
+        directory = Path(directory)
+
+    while directory / ".git" not in directory.iterdir():
+        if directory == Path("/"):
+            raise RuntimeError(f"The directory {directory} is not in a git repo.")
+        else:
+            directory = directory.parent
+    return directory
